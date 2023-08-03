17aae0f417b07c4832898a24a01398bc1ad69ec0
Gekolord <80892021+Gekolord@users.noreply.github.com>
2023-08-03 16:15:43 +0300

    Make a function that removes old grid, make a function that adds new grid based on input, make a function that does these 2 things, but asks for input before

--- index.js
+++ index.js
@@ -12,10 +12,44 @@ const pixels = document.querySelectorAll('.pixel');
 
 pixels.forEach((pixel) => {
     pixel.style.width = `${360 / 16}px`
-    // and for each one we add a 'click' listener
+    // for each one add a 'mouseover' listener
     pixel.addEventListener('mouseover', () => {
         pixel.classList.add('black');
     });
   });
 
+const newGrid = document.querySelector('.newGrid');
 
+// removes a grid of specified selector
+
+function removeGrid (gridSelector, parentNode) {
+    gridSelector.forEach((square) => parentNode.removeChild(square));
+} 
+
+// creates a new grid with number of pixels and class specified 
+
+function createGrid (gridVolume, parentNode, gridClass) {
+    for (let i = 0; i < gridVolume;i++){
+        for (let j = 0; j < gridVolume;j++){
+            let pixel = document.createElement('div');
+            pixel.className = gridClass;
+            parentNode.appendChild(pixel);
+        }
+    }
+}
+
+// ask for pixel volume and remove current grid, then create new one 
+
+function createNewGrid () {
+    let volume = 1
+    while (true)  {
+        volume = prompt("Please specify volume of grid (bigger than 0 and lesser than 100");
+        if (volume > 0 && volume < 101) {
+            break;
+        }
+    }
+    removeGrid(pixels, canvas);
+    createGrid(volume, canvas, "pixel");
+}
+
+createNewGrid()
